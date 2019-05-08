481737dfb5cff33c5d910199b6518be73b55c381
Leijurv <leijurv@gmail.com>
2019-05-08 12:17:56 -0700

    4096

--- util/build_icon.sh
+++ util/build_icon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for ICON in "Icon" "Pepsi"; do
-    for SIZE in "16" "32" "64" "128" "256"; do
+    for SIZE in "16" "32" "64" "128" "256" "512" "1024" "2048" "4096"; do
         echo "Building textures/${ICON}_${SIZE}.png"
         rsvg-convert -h "${SIZE}" "textures/${ICON}.svg" > "textures/${ICON}_${SIZE}.png"
     done
